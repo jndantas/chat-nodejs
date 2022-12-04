e5d87a03d03245226b41e12d0ba93a4d5276a714
Jailton Dantas <jailton.dantass@gmail.com>
2022-12-04 10:32:27 -0300

    finalizado chat

--- app.js
+++ app.js
@@ -1,5 +1,51 @@
 let app = require('./config/server')
-
-app.listen(80, ()=>{
+const { Server } = require('socket.io'); 
+let server = app.listen(80, ()=>{
     console.log('Server On');
+})
+const io = new Server(server);
+app.set('io', io)
+io.listen(server); 
+io.on('connection', (socket)=>{
+    console.log('Usuário conectado');
+
+    socket.on('disconnect', ()=>{
+        console.log('Usuário desconectado');
+    })
+
+    socket.on('sendMsgServer', (data)=>{
+        socket.emit(
+            'msgCliente',
+            {
+                nick: data.nick, 
+                message: data.message 
+            }
+        )
+
+        socket.broadcast.emit(
+            'msgCliente',
+            {
+                nick: data.nick, 
+                message: data.message 
+            }
+        )
+
+        if(parseInt(data.nickUpdated) == 0){
+            socket.emit(
+                'usersChat',
+                {
+                    nick: data.nick
+                }
+            )
+    
+            socket.broadcast.emit(
+                'usersChat',
+                {
+                    nick: data.nick
+                }
+            )
+    
+        }
+        
+    })
 })
--- app/controllers/ChatController.js
+++ app/controllers/ChatController.js
@@ -11,12 +11,19 @@ module.exports.index = (app, req, res)=>{
 }
 
 module.exports.startChat = (app, req, res, errors)=>{
+    let request = req.body
     if(!errors.isEmpty()){
         console.log('erros: ', errors.errors);
         res.render('index', { validations: errors.errors });
         return;
     } else {
-        res.render('chat');
+        app.get('io').emit(
+            'msgCliente',
+            {
+                nick: request.apelido, 
+                message: ' acabou de entrar no chat!' 
+            })
+        res.render('chat', {dataUser: request});
     }
 
 }
